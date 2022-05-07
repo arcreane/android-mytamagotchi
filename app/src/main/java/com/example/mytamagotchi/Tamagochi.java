0cbdc3a8a6d3761ded70490e95d59856c69146b5
Groot <wgirardreydet@gmail.com>
2022-05-07 14:25:55 +0200

    Add lose stat and use create Tamagochi

--- app/src/main/java/com/example/mytamagotchi/Tamagochi.java
+++ app/src/main/java/com/example/mytamagotchi/Tamagochi.java
@@ -1,5 +1,11 @@
 package com.example.mytamagotchi;
 
+import android.util.Log;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Tamagochi {
     public String name;
     public String favoriteObject;
@@ -11,9 +17,13 @@ public class Tamagochi {
     public Integer cleanliness;
     public Integer happiness;
 
-    public Tamagochi(){}
+    public Tamagochi(){
+        Log.i("tag", "azaafazaffaz");
+        this.CreateStatTamagochi("Enzo", "Frites", "Coca", "Ballon", 100, 100, 100, 100,100);
+        this.loseRessources();
+    }
 
-    public Tamagochi(String myName, String myFavoriteFood, String myFavoriteDrink, String myFavoriteObject, Integer myHunger, Integer myThirst, Integer mySleep, Integer myCleanliness, Integer myHappiness){
+    public void CreateStatTamagochi(String myName, String myFavoriteFood, String myFavoriteDrink, String myFavoriteObject, Integer myHunger, Integer myThirst, Integer mySleep, Integer myCleanliness, Integer myHappiness){
         // Starters Data
         name = myName;
         favoriteFood = myFavoriteFood;
@@ -25,6 +35,72 @@ public class Tamagochi {
         cleanliness = myCleanliness;
         happiness = myHappiness;
         // Define in which room the starter is playing based on what is needed for him
+    }
+
+    public void loseRessources () {
+
+        new Timer().scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run(){
+                int random = new Random().nextInt(4);
+                String listOfStat[] = {"hunger", "thrist", "sleep", "cleanliness", "happiness"};
+                switch (listOfStat[random]) {
+                    case "hunger":
+                        if (hunger == 0){
+                            Log.i("tag", "Game over");
+                            break;
+                            /// Swap to game over screen
+                        }
+                        hunger--;
+                        Log.i("tag", "hunger"+ hunger);
+
+                        break;
+                    case "thrist":
+                        if (thirst == 0){
+                            Log.i("tag", "Game over");
+                            break;
+                            /// Swap to game over screen
+                        }
+                        thirst--;
+                        Log.i("tag", "thrist"+thirst);
+
+                        break;
+                    case "sleep":
+                        if (sleep == 0){
+                            Log.i("tag", "Game over");
+                            break;
+                            /// Swap to game over screen
+                        }
+                        sleep--;
+                        Log.i("tag", "sleep" + sleep);
+
+                        break;
+                    case "cleanliness":
+                        if (cleanliness == 0){
+                            Log.i("tag", "Game over");
+                            break;
+                            /// Swap to game over screen
+                        }
+                        cleanliness--;
+                        Log.i("tag", "cleanliness" + cleanliness);
+
+                        break;
+                    case "happiness":
+                        if (happiness == 0){
+                            Log.i("tag", "Game over");
+                            break;
+                            /// Swap to game over screen
+                        }
+                        happiness--;
+                        Log.i("tag", "happiness"+ happiness);
+
+                        break;
+
+
+                }
+                Log.i("tag", "Loses");
+            }
+        },0,10000);
     }
 
 }
